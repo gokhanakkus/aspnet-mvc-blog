b4dffe9ca1bbd2c62f9575655ba803390661f195
NEDİM <NEDM@DESKTOP-JN2RMVD>
2023-05-31 01:35:30 +0300

    Proje dosyası ekle.

---
+++ App.Web.Mvc/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace App.Web.Mvc.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
